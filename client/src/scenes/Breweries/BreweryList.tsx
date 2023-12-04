b8bd96bc4ac70ac9a0fdc6be19531e3bab177526
KaurManjot0432 <0432manjot@gmail.com>
2023-12-04 13:07:20 +0530

    added styles for displaying breweries

--- client/src/scenes/Breweries/BreweryList.tsx
+++ client/src/scenes/Breweries/BreweryList.tsx
@@ -1,84 +1,85 @@
-import BreweryListItem from './BreweryListItem';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import config from '../../config';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setBreweries } from '../../state';
-import { useDispatch } from 'react-redux';
-import Pagination from '@material-ui/lab/Pagination';
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-// import ImagesListItem from './ImagesListItem';
+import BreweryListItem from './BreweryListItem';
+import Box from '@mui/system/Box';
+import Grid from '@material-ui/core/Grid';
+import styled from '@mui/system/styled';
 
 interface Token {
-    token: string;
+  token: string;
 }
 
 interface Breweries {
-    breweries: Brewery[];
+  breweries: Brewery[];
 }
 
 interface Brewery {
-    id: string;
-    name: string;
-    brewery_type: string;
-    address_1: string;
-    address_2: string;
-    address_3: string;
-    city: string;
-    state_province: string;
-    postal_code: string;
-    country: string;
-    longitude: string;
-    latitude: string;
-    phone: string;
-    website_url: string;
-    state: string;
-    street: string;
+  id: string;
+  name: string;
+  brewery_type: string;
+  address_1: string;
+  address_2: string;
+  address_3: string;
+  city: string;
+  state_province: string;
+  postal_code: string;
+  country: string;
+  longitude: string;
+  latitude: string;
+  phone: string;
+  website_url: string;
+  state: string;
+  street: string;
 }
 
+const Item = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  // border: '1px solid',
+  // borderColor: theme.palette.mode === 'dark' ? '#444d58' : '#ced7e0',
+  padding: theme.spacing(1),
+  borderRadius: '4px',
+  textAlign: 'center',
+}));
 
 const BreweryList: React.FC = () => {
+  const dispatch = useDispatch();
+  const token = useSelector((state: Token) => state?.token);
+  const breweries = useSelector((state: Breweries) => state?.breweries);
 
-    const dispatch = useDispatch();
-    const token = useSelector((state: Token) => state?.token);
-    const breweries = useSelector((state: Breweries) => state?.breweries);
+  const fetchBreweries = async () => {
+    try {
+      const url = `${config.apiUrl}/v1/breweries`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const parsedResponse = await response.json();
+      dispatch(setBreweries({ breweries: parsedResponse }));
+    } catch (error) {
+      console.error('Error fetching breweries:', error);
+    }
+  };
 
-    const fetchBreweries = async () => {
-        try {
-            const url = `https://api.openbrewerydb.org/v1/breweries`;
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            const parsedResponse = await response.json();
-            dispatch(setBreweries({ breweries: parsedResponse }));
-            console.log("breweries = ", breweries);
-        } catch (error) {
-            console.error('Error fetching images:', error);
-        }
-    };
+  useEffect(() => {
+    fetchBreweries();
+  }, []);
 
-    useEffect(() => {
-        fetchBreweries();
-    }, []);
-
-    return (
-        <Box sx={{ width: 1, p: 2, overflowY: 'scroll' }}>
-            <div className="container">
-
-                <div className="row">
-                    {breweries && breweries.map((brewery) => {
-                        return <div className="col-md-4">
-                            <BreweryListItem brewery={brewery} />
-                        </div>
-
-                    })}
-                </div>
-            </div>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {breweries &&
+          breweries.map((brewery) => (
+            <Grid xs={6}>
+              <Item><BreweryListItem brewery={brewery} /></Item>
+            </Grid>
+          ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default BreweryList;
--- client/src/scenes/Breweries/BreweryListItem.tsx
+++ client/src/scenes/Breweries/BreweryListItem.tsx
@@ -1,50 +1,139 @@
-import react from 'react';
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Badge, Button, Paper } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { styled } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+import PhoneIcon from '@mui/icons-material/Phone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Box from '@mui/system/Box';
+import LinkIcon from '@mui/icons-material/Link';
+import Rating from '@mui/material/Rating';
 
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 interface BreweryItemProps {
-    brewery: {
-        id: string;
-        name: string;
-        brewery_type: string;
-        address_1: string;
-        address_2: string;
-        address_3: string;
-        city: string;
-        state_province: string;
-        postal_code: string;
-        country: string;
-        longitude: string;
-        latitude: string;
-        phone: string;
-        website_url: string;
-        state: string;
-        street: string;
-    };
+  brewery: {
+    id: string;
+    name: string;
+    brewery_type: string;
+    address_1: string;
+    address_2: string;
+    address_3: string;
+    city: string;
+    state_province: string;
+    postal_code: string;
+    country: string;
+    longitude: string;
+    latitude: string;
+    phone: string;
+    website_url: string;
+    state: string;
+    street: string;
+  };
 }
 
-const BreweryListItem : React.FC<BreweryItemProps> = ({ brewery })  => {
-    return (
-        <div my-3>
-        <div className="card">
-        <div style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                position: 'absolute',
-                right: '0'
-            }
-            }>
+const BreweryListItem: React.FC<BreweryItemProps> = ({ brewery }) => {
+  const [expanded, setExpanded] = React.useState(false);
 
-                <span className="badge rounded-pill bg-danger"> {brewery.brewery_type} </span>
-            </div>
-            <div className = "card-body">
-            <h5 className = "card-title">{brewery.name}</h5>
-            <p className = "card-text">{brewery.address_1}</p>
-            <p className="card-text"><small className="text-muted">By {brewery.country}</small></p>
-            <a href={brewery.website_url}   target="_blank" className = "btn btn-sm btn-dark">Read more</a>
-            </div>
-        </div>
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const getAvatarBackgroundColor = (name: string): string => {
+    // Find the first alphabet in the name
+    const firstAlphabet = name.match(/[a-zA-Z]/);
+
+    if (firstAlphabet) {
+      const charCode = firstAlphabet[0].charCodeAt(0);
+      const colorIndex = charCode % 5;
+      const colors = [red[500], 'blue', 'green', 'orange', 'purple'];
+      return colors[colorIndex];
+    }
+
+    // Return a default color if no alphabet is found
+    return 'red';
+  };
+  const avatarBackgroundColor = getAvatarBackgroundColor(brewery.name);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Paper elevation={4} sx={{ width: 500, margin: 2 }}>
+        <Card sx={{ boxShadow: 4 }}>
+          <CardHeader
+            avatar={<Avatar sx={{ bgcolor: avatarBackgroundColor }}>{brewery.name.match(/[a-zA-Z]/)}</Avatar>}
+            action={<IconButton aria-label="settings"><MoreVertIcon /></IconButton>}
+            title={<Typography variant="h6">{brewery.name}</Typography>}
+            subheader={<Typography variant="subtitle1">{`${brewery.state}, ${brewery.country}`}</Typography>}
+          />
+
+          <CardContent>
+            <Box display="flex" alignItems="center">
+              <PhoneIcon sx={{ mr: 1 }} />
+              <Typography fontSize="16px" color="textSecondary" gutterBottom>
+                {brewery.phone}
+              </Typography>
+            </Box>
+
+            <Box display="flex" alignItems="center">
+              <LocationOnIcon sx={{ mr: 1 }} />
+              <Typography fontSize="16px" color="textSecondary" gutterBottom>
+                {brewery.address_1}, {brewery.city}
+              </Typography>
+            </Box>
+
+            <Box display="flex" alignItems="center">
+              <Link href={brewery.website_url} target="_blank" underline="hover">
+                <Typography>
+                  <LinkIcon style={{ color: 'black' }} /> Visit
+                </Typography>
+              </Link>
+            </Box>
+
+            <Box display="flex" alignItems="center">
+              <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+            </Box>
+
+          </CardContent>
+
+          <CardActions disableSpacing>
+            <Typography variant="h6" margin="2px" fontSize="20px">
+              Reviews
+            </Typography>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+
+          </Collapse>
+        </Card>
+      </Paper>
     </div>
-    )
-}
+  );
+};
 
 export default BreweryListItem;
